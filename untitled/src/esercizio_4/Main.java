8060e00811f7fcd1c0c19e4f8b99d8ee7ea5e21e
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-23 17:44:53 +0200

    completed exercise four

--- untitled/src/esercizio_4/Main.java
+++ untitled/src/esercizio_4/Main.java
@@ -1,7 +1,20 @@
 package esercizio_4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        // Facciamo selezionare un numero all'utente
+        System.out.println("Inserisci un numero:");
+        int numero = Integer.parseInt(scanner.nextLine());
+
+        // Ciclo che stampa i numeri da numero a 0
+        for (int i = numero; i >= 0; i--) {
+            System.out.println(i);
+        }
+
+        scanner.close();
     }
 }
