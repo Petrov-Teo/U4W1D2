dad8f43e6125290bdc97ffaeb7e63557bb16069f
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-23 15:23:28 +0200

    fist commit

---
+++ untitled/src/esercizio_2/Main.java
@@ -0,0 +1,7 @@
+package esercizio_2;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ untitled/src/esercizio_4/Main.java
@@ -0,0 +1,7 @@
+package esercizio_4;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
