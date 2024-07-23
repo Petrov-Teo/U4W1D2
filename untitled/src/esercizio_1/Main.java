62b1e169882be5aee71beefbfc35da49ffecab80
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-23 16:19:39 +0200

    completed exercise one and two

--- untitled/src/esercizio_1/Main.java
+++ untitled/src/esercizio_1/Main.java
@@ -1,7 +1,30 @@
 package esercizio_1;
 
+
 public class Main {
     public static void main(String[] args) {
-     
+        // Richiamato il primo metodo per verificare se un numero è pari o dispari
+        padiDispari(4); //<—— il numero va qui
+
+//Richiamiamo il metodo e passiamo il parametro richiesto un anno a tua scelta
+        annoBisestile(1728);//  <—— Inseriamo l'anno da verificare
+    }
+
+    //Metodo Pari Dispari con il costrutto IF ELSE
+    public static void padiDispari(int numero) {
+        if (numero % 2 == 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+    //Metodo per verificare se l'anno è bisestile
+    public static void annoBisestile(int year) {
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
     }
 }
--- untitled/src/esercizio_2/Main.java
+++ untitled/src/esercizio_2/Main.java
@@ -1,7 +1,33 @@
 package esercizio_2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Dammi il primo numero");
+        int numero = Integer.parseInt(scanner.nextLine());
+        switch (numero) {
+            case 1:
+
+                System.out.println("uno");
+                break;
+            case 2:
+
+                System.out.println("due");
+                break;
+            case 3:
+
+                System.out.println("tre");
+                break;
+            default:
+                System.out.println("il numero non e presente nei possibili casi ");
+                break;
+
+        }
+        scanner.close();
     }
+
+
 }
