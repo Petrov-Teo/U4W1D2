06892692cebe42d607b1cf44916fea24996cda21
Petrov-Teo <168548498+Petrov-Teo@users.noreply.github.com>
2024-07-23 16:37:53 +0200

    completed exercise three

--- untitled/src/esercizio_3/Main.java
+++ untitled/src/esercizio_3/Main.java
@@ -1,7 +1,23 @@
 package esercizio_3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Inserisci una parola");
+        String text = scanner.nextLine();
+
+        StringBuilder newText = new StringBuilder();
+        int i = 0;
+        while (i < text.length()) {
+            newText.append(text.charAt(i));
+            if (i < text.length() - 1) {
+                newText.append(',');
+            }
+            i++;
+        }
+        System.out.println(newText.toString());
     }
 }
